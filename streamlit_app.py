69d01d6547d7c8e257308a6cf584436a663f37d7
Júlio César de Andrade Macedo <98000665+juliomacedobsb@users.noreply.github.com>
2023-09-18 12:24:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
 
-
 streamlit.title('My parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 avogado Toast') 
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
